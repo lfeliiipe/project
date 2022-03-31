8e1e55bbdb10b2acbf61172a23e7088f0b7b2a04
Luiz Felipe <lfeliiipe@hotmail.com>
2022-03-31 17:48:45 -0300

    Add block sites from black list

--- background.js
+++ background.js
@@ -1,5 +1,11 @@
-let onfocus = false;
-
+// Save inZone variable using storage API
+let inZone = false;
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ onfocus });
+    chrome.storage.sync.set({ inZone });
+});
+
+// Save blackList variable using storage API
+const blackList = ["twitter.com", "facebook.com", "youtube.com"];
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ blackList });
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 // Initialize button with user's preferred color
 let b1 = document.getElementById("b1");
 
-// Change b1 color background based on storage variable onfocus
+// Change b1 color background based on storage variable inZone
 function changeButtonColor() {
-    chrome.storage.sync.get("onfocus", ({ onfocus }) => {
-        if (onfocus) {
+    chrome.storage.sync.get("inZone", ({ inZone }) => {
+        if (inZone) {
             b1.style.backgroundColor = "red";
             b1.innerHTML = "IN THE ZONE";
         } else {
@@ -15,11 +15,37 @@ function changeButtonColor() {
 }
 changeButtonColor();
 
-// When the button is clicked change storage variable onfocus and button color
+// When the button is clicked change storage variable inZone and button color
 b1.addEventListener("click", function () {
-    chrome.storage.sync.get("onfocus", ({ onfocus }) => {
-        onfocus = !onfocus;
-        chrome.storage.sync.set({ onfocus });
+    chrome.storage.sync.get("inZone", ({ inZone }) => {
+        inZone = !inZone;
+        chrome.storage.sync.set({ inZone });
         changeButtonColor();
     });
 });
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+b1.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: blockSites,
+    });
+});
+  
+// The body of this function will be executed as a content script inside the
+// current page
+function blockSites() {
+    chrome.storage.sync.get("blackList", ({ blackList }) => {
+        // Check if the current page is not in the black list
+        blackList.forEach(function (value) {
+            alert("item: " + value + " da blacklist e hostname: " + window.location.hostname + " são iguais: " + 
+            (window.location.hostname.includes(value)));
+            if (window.location.hostname.includes(value)) {
+                alert("eh pra redirecionar");
+                window.location.href = "chrome-extension://hknjekbcijhgckhennogkgpigbndefph/block.html";
+            }
+        });
+    });
+}
